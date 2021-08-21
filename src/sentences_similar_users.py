35203a59fb09acd9de3616089ce015bbe4b4029e
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-21 11:37:00 +0545

    clean text function removed

--- src/sentences_similar_users.py
+++ src/sentences_similar_users.py
@@ -1,49 +1,10 @@
-import re
 import nltk
 import numpy as np
 import pandas as pd
 from nltk.corpus import stopwords
 from sentence_transformers import SentenceTransformer, util
 
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
-
-
-def main():
-
-    df = pd.read_csv('F:/Leapfrog Technology/Project 4/data/profile.csv', header=0)
-    clean_text = []
-    for index in range(len(df['description'])):
-        text = df['description'][index]
-        text = text.lower()
-        text = re.sub(r'\[[0-9]*\]', ' ', text)
-        text = re.sub(r'\s+', ' ', text)
-        text = re.sub(r'\d', ' ', text)
-        text = re.sub(r'\s+', ' ', text)
-        words = text.lower().split()
-        stop_word_set = set(stopwords.words("english"))
-        cleaned_words = list(set([w for w in words if w not in stop_word_set]))
-        cleaned_words = [' '.join(cleaned_words[::])]
-        clean_text.append(cleaned_words[0])
-    df.insert(3, "cleaned_words", clean_text, True)
-
-    return df
-
-
-def clean_input(input_text):
-
-    process_text = input_text.lower()
-    process_text = re.sub(r'\[[0-9]*\]', ' ', process_text)
-    process_text = re.sub(r'\s+', ' ', process_text)
-    process_text = re.sub(r'\d', ' ', process_text)
-    process_text = re.sub(r'\s+', ' ', process_text)
-    process_words = process_text.lower().split()
-    stop_words = set(stopwords.words("english"))
-    cleaned_input = list(set([w for w in process_words if w not in stop_words]))
-    cleaned_input = [' '.join(cleaned_input[::])]
-
-    return cleaned_input
-
-
+# model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 def similarities(df, new_des):
 
     scores = {}
