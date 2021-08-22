c457c2d8068c4df509729cfca86c0b8b6bb420e3
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-22 14:09:19 +0545

    error handling

--- src/similarity_jaccard.py
+++ src/similarity_jaccard.py
@@ -2,18 +2,21 @@ def jaccard_similarity(list1, list2):
     """Calculates the jaccard similarity of two lists.
 
     Parameters
-    ----------
+    ------------------------
     list1 : [list]
         [a list containing a sentence]
     list2 : [list]
         [a list containing a sentence]
 
     Returns
-    -------
+    ------------------------
     [float]
         [returns the similarity score nin float]
     """
     intersection = len(set(list1).intersection(list2))
     union = len(set(list1)) + len(set(list2)) - intersection
-
-    return intersection / union
+    try:
+        similarity_score = intersection / union
+    except:
+        similarity_score = "Similarity score can't be calculated."
+    return similarity_score
