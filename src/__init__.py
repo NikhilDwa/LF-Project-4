e1b90d0191d7e2eb2120de3ed405d90aba2d8af6
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-22 14:06:18 +0545

    import required function

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .sentence_clean import clean_text
+from .similarity_tfidf import tfidf_vector
+from .similarity_hugging_face import similar_check
+from .similarity_jaccard import jaccard_similarity
